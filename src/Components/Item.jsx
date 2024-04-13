63f56ef1ebbe618172c7faeb953ad68307cc23a0
mahya <beheshtimahya11@gmail.com>
2024-04-13 21:01:52 +0330

    finall

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
 export const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    backgroundColor: "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: "center",
